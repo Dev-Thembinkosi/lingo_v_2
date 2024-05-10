ea0f8fa41cfda6a0cc675a2775d6530ca0954ccc
Dev-Thembinkosi <manhique@gmail.com>
2024-05-10 11:38:33 +0200

    network fix

---
+++ app/buttons/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button"
+
+const ButtonsPage = () =>{
+    return (
+        <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
+            <Button>Default button</Button>
+            <Button variant="primary">Primary </Button>
+            <Button variant="primaryOutline">Primary Outline</Button>
+            <Button variant="secondary">Primary </Button>
+            <Button variant="secondaryOutline">Primary Outline</Button>
+            <Button variant="danger">Danger</Button>
+            <Button variant="dangerOutline">Danger Outline</Button>
+            <Button variant="super">Super</Button>
+            <Button variant="superOutline">Super Outline</Button>
+            <Button variant="guost">Guost</Button>
+            <Button variant="sidebar">SideBar</Button>
+            <Button variant="sidebarOutline">Super Outline</Button>
+      </div>
+    );
+}; 
+export default ButtonsPage;
